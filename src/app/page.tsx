7d346503a54a5424fdcacdb6e310ad45521f34b6
nicov <nicovenz@gmail.com>
2024-12-12 17:58:38 -0300

    refactor: change ui labels and colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,7 +23,7 @@ const Home: React.FC = () => {
             <header className="w-full py-6 bg-zinc-800 fixed top-0 left-0 z-50">
                 <div className="max-w-7xl mx-auto flex flex-col sm:flex-row justify-between items-center px-4 sm:px-8">
                     <h1 className="text-center text-3xl sm:text-4xl font-bold text-zinc-200 break-words sm:break-normal">
-                        Welcome to AI URL Chatbot
+                        Meet &lt;<span className="text-blue-500">Milo</span>/&gt;
                     </h1>
                     <nav className="flex flex-col sm:flex-row gap-4 sm:gap-6 mt-4 sm:mt-0">
                         <a
@@ -56,19 +56,17 @@ const Home: React.FC = () => {
 
             <div
                 className="pt-24 flex flex-col absolute items-center justify-center w-full h-full sm:pt-32 px-4 sm:px-6">
+                <h2 className="text-center text-3xl sm:text-4xl font-bold text-zinc-200 break-words sm:break-normal mb-8">
+                    Let's <span className="text-blue-500">Dive In</span>
+                </h2>
                 <p className="text-zinc-400 max-w-full sm:max-w-lg mx-auto mb-8 text-center">
-                    Enter any webpage URL below, and you will be redirected to chat with our AI chatbot. The bot will
-                    provide
-                    detailed insights and information about the webpage you entered.
+                    Enter a website below, and Milo will be ready to chat and provide insights about the site!
                 </p>
 
                 <form
                     onSubmit={handleSubmit}
                     className="bg-zinc-800 p-6 sm:p-8 rounded-lg shadow-md w-full sm:w-96 max-w-full mx-auto"
                 >
-                    <label htmlFor="url" className="text-center block text-zinc-200 text-sm font-medium mb-2">
-                        Enter Webpage URL
-                    </label>
                     <input
                         type="url"
                         id="url"
--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -14,9 +14,12 @@ export const Message = ({content, isUserMessage}: MessageProps) => {
         <div className="p-6">
             <div className="max-w-3xl mx-auto flex items-start gap-2.5">
                 <div
-                    className={cn("size-10 shrink-0 aspect-square rounded-full border border-zinc-700 gb-zinc-900 flex justify-center items-center",
+                    className={cn("size-10 shrink-0 aspect-square rounded-full border border-blue-300 gb-zinc-900 flex justify-center items-center",
                         {
-                            "bg-blue-950 border-blue-700 text-zinc-200": isUserMessage
+                            "bg-zinc-950 border-zinc-500 text-zinc-200": isUserMessage
+                        },
+                        {
+                            "bg-blue-500": !isUserMessage
                         }
                     )}>
                     {isUserMessage ? <User className="size-5"/> : <Bot className="size-5 text-white"/>}
@@ -24,7 +27,7 @@ export const Message = ({content, isUserMessage}: MessageProps) => {
                 <div className="flex flex-col ml-6 w-full">
                     <div className="flex items-center space-x-2">
                         <span className="text-sm font-semibold text-gray-900 dark:text-white">
-                            {isUserMessage ? "You" : "Website"}
+                            {isUserMessage ? "You" : "Milo"}
                         </span>
                     </div>
                     <p className="text-sm font-normal py-2.5 text-gray-900 dark:text-white">{content}</p>
